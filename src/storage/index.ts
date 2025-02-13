c97d328fa11b908f57995ecb9b89e38965fbf84a
Jackie Lu <jackielu3@gmail.com>
2025-02-12 16:39:37 -0800

    uhrp-url added and changed to StorageUtils

---
+++ src/storage/index.ts
@@ -0,0 +1,8 @@
+export { default as StorageUtils } from './StorageUtils.js'
+export {
+    normalizeURL,
+    getURLForHash,
+    getURLForFile,
+    getHashFromURL,
+    isValidURL
+} from './StorageUtils.js'
